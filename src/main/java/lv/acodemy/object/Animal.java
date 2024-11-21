93de1623633b8583801027082e6ce981e6498a13
LIPINAOLHA <lipina.helga@gmail.com>
2024-11-21 20:50:52 +0200

    Added Car class and updated ObjectExample

---
+++ src/main/java/lv/acodemy/object/Animal.java
@@ -0,0 +1,8 @@
+package lv.acodemy.object;
+
+public class Animal {
+
+   public void eat (){
+        System.out.println( "Eating ...");
+    }
+}
---
+++ src/main/java/lv/acodemy/object/Dog.java
@@ -0,0 +1,8 @@
+package lv.acodemy.object;
+
+public class Dog  extends Animal {
+   public void bark () {
+        System.out.println( "Barking ...");
+
+    }
+}
